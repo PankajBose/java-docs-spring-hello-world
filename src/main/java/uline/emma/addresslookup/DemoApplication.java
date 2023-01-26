1d63afa5b7a72ad6dfd49e4e91825efcdfde8473
PankajBose <bosepankaj@gmail.com>
2023-01-26 15:07:09 +0530

    changed project

--- src/main/java/uline/emma/addresslookup/DemoApplication.java
+++ src/main/java/uline/emma/addresslookup/DemoApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package uline.emma.addresslookup;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
